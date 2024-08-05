71adaf298a45eb4cc516324869df2f0e82cc02df
E-Duchac <emmagduchac@outlook.com>
2024-08-04 23:14:07 -0400

    SecurityConfig class added; adjustments made to pom.xml for spring boot dependencies to avoid contradicting each other. Confirmed functionality with mvn clean install and by running.

--- thinkfast/src/main/java/com/emma/thinkfast/config/SecurityConfig.java
+++ thinkfast/src/main/java/com/emma/thinkfast/config/SecurityConfig.java
@@ -1,21 +1,49 @@
 package com.emma.thinkfast.config;
 
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    // private final UserDetailsService userDetailsService;
 
-    //This bean is specifically to get around auth requirements for the login and anonymous landing page.
-    // @Bean
-    // public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    //     http.authorizeHttpRequests(requests -> 
-    //         requests
-    //             .requestMatchers("/user/login").permitAll()
-    //             .anyRequest().authenticated()
-    //         );
-    //     return http.build();
+    // public SecurityConfig(UserDetailsService userDetailsService) {
+    //     this.userDetailsService = userDetailsService;
     // }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+            authorizeRequests
+                // Adjust to the following when Login system is finished:
+                //.antMatchers("/register").permitAll()     //Registry is any-access
+                //.anyRequest().authenticated()             //Other requests require auth
+                
+                .anyRequest().permitAll()                   //Allows all requests w/out auth
+            )
+            .formLogin(formLogin ->
+                formLogin
+                    .loginPage("/login")    //Custom login
+                    .permitAll()                      //Login is any-access
+            )
+            .logout(LogoutConfigurer::permitAll //Logout is any-access
+            );
+        //Disable csrf, also but only during development.
+        http.csrf().disable();
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
