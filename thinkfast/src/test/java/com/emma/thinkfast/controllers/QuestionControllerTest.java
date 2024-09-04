75dcfcfeb49bc257ed23aff59de52578d82807b8
E-Duchac <emmagduchac@outlook.com>
2024-09-03 22:07:55 -0400

    Working on QuestionControllerTest.

---
+++ thinkfast/src/test/java/com/emma/thinkfast/controllers/QuestionControllerTest.java
@@ -0,0 +1,82 @@
+package com.emma.thinkfast.controllers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.emma.thinkfast.dtos.CustomQuizRequest;
+import com.emma.thinkfast.enums.Category;
+import com.emma.thinkfast.models.Question;
+import com.emma.thinkfast.services.QuestionServiceImpl;
+
+class QuestionControllerTest {
+    @Mock
+    private QuestionServiceImpl questionService;
+
+    private QuestionController questionController;
+
+    private Question question;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+        questionController = new QuestionController(questionService);
+
+        question = new Question();
+        question.set_id("ignore-me-question-id");
+        question.setQuestionText("Alexander the Great remarked of this historical figure that if he could not be Alexander the Great, he would be this man, a personal acquaintence. In return, this man responded that if he could not be himself, he would <i>also</i> choose to be himself. Which Ancient Greek philosopher was best known for his search \"for an honest man\"?");
+        List<String> answerList = new ArrayList<String>();
+        answerList.add("Diogenes");
+        question.setAnswerText(answerList);
+        question.setCategory(Category.SOCIAL_STUDIES);
+    }
+
+    @Test
+    void testSaveQuestion() {
+        ResponseEntity<String> responseEntity = questionController.saveQuestion(question);
+
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    void testGetQuestionById() {
+        when(questionService.getQuestionById(question.get_id())).thenReturn(question);
+
+        ResponseEntity<String> responseEntity = questionController.getQuestionById(question.get_id());
+
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    void testGetQuestionsByCategory() {
+        List<Question> questionsByCategory = new ArrayList<Question>();
+        
+        when(questionService.getQuestionsByCategory(Category.SOCIAL_STUDIES)).thenReturn()
+    }
+
+    @Test
+    void testGetQuestionsByMultiCategory() {
+        CustomQuizRequest customRequest;
+    }
+
+    @Test
+    void testUpdateQuestion() {
+
+    }
+
+    @Test
+    void testDeleteQuestion() {
+
+    }
+}
