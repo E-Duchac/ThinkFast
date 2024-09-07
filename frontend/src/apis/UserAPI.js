e738332fceb90b718c19b8ffc70420c5fad882fa
E-Duchac <emmagduchac@outlook.com>
2024-09-06 20:19:56 -0400

    Starting work on the Login function; reveals some issues in translating and retrieving User objects as demonstrated in failing User-related tests. Going to consider how to proceed overnight.

--- frontend/src/apis/UserAPI.js
+++ frontend/src/apis/UserAPI.js
@@ -1,1 +1,21 @@
 const userUrl = process.env.REACT_APP_USER_URL;
+
+export const login = async (username, password) => {
+    const loginRequestBody = {username: username, password: password};
+    try {
+        const response = await fetch(userUrl + "/login", {
+            method: 'POST',
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify(loginRequestBody)
+        });
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log('Parsed JSON: ', data);
+        return data;
+    } catch (error) {
+        console.error('Error fetching user: ', error);
+        throw error;
+    }
+}
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,7 +1,38 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { login } from '../apis/UserAPI';
 
-function Login() {
+const Login = () => {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [token, setToken] = useState();
+
+    const loginUser = async (event) => {
+        event.preventDefault();
+        const setToken = await Promise.resolve(login(username, password));
+        console.log("Token: " + token);
+        //set username, profile, etc.
+        //navigate
+    }
+
     return (
-        <p>Stubbed login page. <a href="http://localhost:3000/">Go back.</a></p>
+        <div className="App">
+            <Header />
+            <div className="App-body">
+                <p>Stubbed login page. <a href="http://localhost:3000/">Go back.</a></p>
+                <div className="login-panel">
+                    <form className="login-form" onSubmit={loginUser}>
+                        <input type="text" value={username} onChange={e => setUsername(e.target.value)}></input>
+                        <input type="text" value={password} onChange={e => setPassword(e.target.value)}></input>
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            </div>
+            <Footer />
+        </div>
     );
 }
 
